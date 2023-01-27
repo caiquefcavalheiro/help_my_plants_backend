310e9508392b1b2d55ffa876780e68475811ced7
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-01-26 23:43:15 -0300

    feat: config allowed host to railway

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-d7d5.up.railway.app"]
 
 
 # Application definition

cfc3373db6c5d62d2d9754de5dbbafb7f3a939b3
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-01-27 20:55:15 -0300

    fix: fixed to have only one admin user with permissions to get, post, patch and delete new plants

--- users/serializers.py
+++ users/serializers.py
@@ -8,7 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"id": {"read_only" : True}, "password": {"write_only": True}}
 
     def create(self, validated_data: dict) -> User:
-        return User.objects.create_superuser(**validated_data)
+        if validated_data["email"] == "caique@email.com.br":
+            return User.objects.create_superuser(**validated_data)
+        else:
+            user = User.objects.create(**validated_data)
+            user.set_password(validated_data["password"])
+            user.save()
+            return user
 
 
     def update(self, instance, validated_data):

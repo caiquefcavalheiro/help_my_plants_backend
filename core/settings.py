a373119e50d4f7f5f74ad45700436951b24abccb
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-01-27 16:20:27 -0300

    feat: change cors allowed origins

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["web-production-d7d5.up.railway.app", "127.0.0.1", "*"]
+# ALLOWED_HOSTS = ["web-production-d7d5.up.railway.app", "127.0.0.1", "*"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -83,7 +84,8 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = ["*/"]
+# CORS_ALLOWED_ORIGINS = ["*/"]
+
 
 ROOT_URLCONF = 'core.urls'
 

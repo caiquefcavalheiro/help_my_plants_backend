bbf85b0909a204f2cde040e860caa8bcf54e5374
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-04-17 17:34:42 -0300

    feat: change allowedHosts

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-get_hosts = json.loads(os.getenv("ALLOWED_HOSTS", ["*"]))
+get_hosts = json.loads(os.getenv("ALLOWED_HOSTS", []))
 
 ALLOWED_HOSTS = get_hosts
 

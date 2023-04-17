fcafc2fcd47088872ba36bb0b1ddb8340fb654c0
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-04-17 16:09:08 -0300

    fixed: allowed hosts reading string of hosts

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", [])
+get_hosts = os.getenv("ALLOWED_HOSTS", []).split()
+
+ALLOWED_HOSTS = get_hosts
+
 
 # Application definition
 APPEND_SLASH = False
